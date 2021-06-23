687e5cf38760f92e2282ff322bec196f226b2a80
erripollo <pollone.enrico@gmail.com>
2021-06-23 12:48:46 +0200

    Complete snack 2 blocco 2

---
+++ Blocco_2/Snack_2/list.php
@@ -0,0 +1,31 @@
+<?php 
+
+$invitati = ['enrico', 'andrea', 'giulia', 'alessia', 'fabio'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h2>
+        <?php 
+        if (in_array($_GET['name'], $invitati)) {
+
+            echo 'ok';
+        }else{
+            echo 'ko';
+        };
+        ?>
+    
+    </h2>
+
+
+</body>
+</html>
